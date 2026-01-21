620049f3e71f4eff4899bef70e5c6ea7d79dd1d8
Saman Pandey <singularity.1@hotmail.com>
2026-01-22 00:25:37 +0530

    style: get rid of redundancies

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = ({ className }) => {
   return (
     <div className={`flex items-center gap-2 ${className}`}>
       <img src={logoSrc} alt="Logo" className="w-8 h-8" />
-      <span className="font-bold text-lg">AppName</span>
+      <span className="font-bold text-lg" style={{ color: 'var(--text)' }}>RelayOps</span>
     </div>
   )
 }
