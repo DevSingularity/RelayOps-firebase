2b8a9a1c183f11809440f06cbb6ef9c88b752ed3
Saman Pandey <singularity.1@hotmail.com>
2026-01-28 17:25:02 +0530

    feat: add layout components

---
+++ client/src/components/layout/Navbar.jsx
@@ -0,0 +1,44 @@
+import { SearchIcon, PanelLeft } from 'lucide-react'
+import { useDispatch, useSelector } from 'react-redux'
+import { assets } from '../../assets/assets'
+import ThemeToggle from '../theme/ThemeToggle'
+
+const Navbar = ({ setIsSidebarOpen }) => {
+
+  const dispatch = useDispatch();
+  const { theme } = useSelector(state => state.theme);
+
+  return (
+    <div className="w-full bg-white dark:bg-zinc-900 border-b border-gray-200 dark:border-zinc-800 px-6 xl:px-16 py-3 flex-shrink-0">
+      <div className="flex items-center justify-between max-w-6xl mx-auto">
+        {/* Left section */}
+        <div className="flex items-center gap-4 min-w-0 flex-1">
+          {/* Sidebar Trigger */}
+          <button onClick={() => setIsSidebarOpen((prev) => !prev)} className="sm:hidden p-2 rounded-lg transition-colors text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-zinc-800" >
+            <PanelLeft size={20} />
+          </button>
+
+          {/* Search Input */}
+          <div className="relative flex-1 max-w-sm">
+            <SearchIcon className="absolute left-2.5 top-1/2 -translate-y-1/2 text-gray-400 dark:text-zinc-400 size-3.5" />
+            <input
+              type="text"
+              placeholder="Search projects, tasks..."
+              className="pl-8 pr-4 py-2 w-full bg-white dark:bg-zinc-900 border border-gray-300 dark:border-zinc-700 rounded-md text-sm text-gray-900 dark:text-white placeholder-gray-400 dark:placeholder-zinc-400 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 transition"
+            />
+          </div>
+        </div>
+
+        {/* Right section */}
+        <div className="flex items-center gap-3">
+          <ThemeToggle />
+
+          {/* User Button */}
+          <img src={assets.profile_img_a} alt="User Avatar" className="size-7 rounded-full" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
